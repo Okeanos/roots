ebf4f60619457a290d45d02ee044cc5ee0108efc
Nikolas Grottendieck <bitbucket@nikolasgrottendieck.com>
2013-12-04 17:37:00 +0100

    Added a custom function and template to suppress any comment related output on public facing pages/articles/anything.

--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,13 @@
 /**
  * Custom functions
  */
+
+add_filter( 'comments_template', 'roots_custom_remove_comments' );
+/**
+ * Removes all public facing comment interfaces, that includes getting rid of "Comments are closed" messages.
+ * Even if comments are turned on in Wordpress they will not be rendered anywhere.
+ * @param $file path to current comments file
+ */
+function roots_custom_remove_comments( $file ) {
+	return get_template_directory() . '/templates/comments-empty.php';
+}
